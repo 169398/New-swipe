37b01b0c6f9fa06ff6f09261b7e28b50690045d4
169398 <kulubiidris@gmail.com>
2024-05-03 14:19:17 +0300

    added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Header2 } from '@/components/common/header2'
+import { Navbar } from '@/components/common/navbar'
 
 export default function Home() {
   return (
     <>
+    <Navbar/>
       <Header2 />
     </>
   )
--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -13,19 +13,17 @@ export function Footer() {
           >
             <img src="/logo.svg" className="mr-3" />
             <Typography className="!text-white !text-base font-medium ">
-              Pandem
+             swipe
             </Typography>
           </Link>
         </div>
         <div className="flex max-w-fit items-center gap-x-4">
           <Link
-            href="https://map.sistilli.dev/public/coding/SaaS+Boilerplate"
+            href="/"
             target="_blank"
             className="pointer block w-fit flex-1"
           >
-            <Typography variant="p" className="w-max">
-              Book a demo
-            </Typography>
+            
           </Link>
           <Link
             href="/terms-of-service"
